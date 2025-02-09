8eea063ae6258c8330fa8fe56985510f5f6f119f
Shah Mohammad Ansari <shahmohammadt01@gmail.com>
2025-02-09 18:50:18 +0530

    episode-5, part-3

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,25 @@
-import resList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = ()=> {
 
     // Hook - useState
-    const [listOfRestaurant, setListOfRestaurant] = useState(resList);
+    const [listOfRestaurant, setListOfRestaurant] = useState([]);
 
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async ()=> {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        setListOfRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+        // console.log(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+
+    if(listOfRestaurant.length === 0) return <Shimmer/>
+    
 
     return (
         <div className="Body-container">
@@ -18,12 +31,6 @@ const Body = ()=> {
                     setListOfRestaurant(filteredList);
                  } }
                 >Top Rated Restaurant</button>
-                <button
-                className="all-res-btn"
-                onClick={() => {
-                    setListOfRestaurant(resList);
-                }}
-                >See All Restaurant</button>
             </div>
             <div className="res-container">
                 {listOfRestaurant.map( (restaurant) => (
