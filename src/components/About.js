cbaddaf654cb149a805e6d57440c3aa9a3aa356e
Shah Mohammad Ansari <shahmohammadt01@gmail.com>
2025-02-24 16:46:51 +0530

    episode-8, part-1, class based component & props,state

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About = ()=> {
     return(
         <div>
             <h2>About us</h2>
             <h3>This is about page</h3>
+
+            <User name={"Shah, (Function)"} location={"Rampur"} email={"shahmohammadt01@gmail.com"}/>
+
+            <UserClass name={"Shah, (Class)"} location={"Rampur"} email={"shahmohammadt01@gmail.com"}/>
         </div>
     )
 }
---
+++ src/components/User.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const User = ({name, location, email})=> {
+    const [count, setCount] = useState(0);
+    return (
+        <div className="user-card">
+            <h1>Count: {count}</h1>
+            <button
+            onClick={()=>{setCount(count+1)}}
+            >Count Increase</button>
+            <h2>Name: {name}</h2>
+            <h3>Location: {location}</h3>
+            <h4>Email: {email}</h4>
+        </div>
+    )
+}
+
+export default User;
